a7bb161dc5a4cd9028118ddfd3dbeb8a06cb0790
Hérick Portugues <herick.portugues@gmail.com>
2023-02-06 00:44:00 -0300

    Inicia implementação do grafo com a lib react-cytoscapejs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import data from './data/data.json';
+import CytoscapeComponent from 'react-cytoscapejs';
 
-function App() {
+const stylesheet = [
+  {
+    selector: 'node',
+    style: {
+      width: 50,
+      height: 50,
+      "font-size": "50px",
+      label: 'data(label)',
+    },
+  },
+  {
+    selector: 'edge',
+    style: {
+      width: 10,
+      "font-size": "50px",
+      label: 'data(label)',
+    },
+  },
+];
+
+const buildElements = () => {
+  let elements = []
+
+  const screenWidth = window.screen.width
+  const screenHeight = window.screen.width
+
+  for (let i = 0; i < data.length; i++) {
+    elements.push({ data: { id: i, label: data[i].name }, position: { x: screenWidth * 0.1 * data[i].position_x, y: screenHeight * 0.1 * data[i].position_y } })
+
+    // for (let j = i; j < data[i].edges.length; j++) {
+    //   if (data[i].edges[j] !== 0) {
+    //     elements.push({ data: { source: i, target: j, label: `${data[i].edges[j]} m` }, selectable: false })
+    //   }
+    // }
+  }
+
+  console.log(elements)
+
+  return elements
+}
+
+export default function App() {
+  const elements = buildElements()
+
+  const setListeners = (cy) => {
+    // example cytoscape event listener
+    cy.on('mouseover', 'edge', (event) => {
+      console.log(cy)
+    });
+  };
+
+  console.log(window.screen.width)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main>
+        <CytoscapeComponent
+          elements={elements}
+          // autolock
+          style={{ width: '100vw', height: '80vh' }}
+          layout={{ name: 'preset' }}
+          stylesheet={stylesheet}
+          cy={(cy) => {
+            setListeners(cy);
+          }}
+        />
+      </main>
     </div>
   );
 }
-
-export default App;
